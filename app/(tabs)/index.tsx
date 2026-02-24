f452cd310e83f2a9012799bbc6db75695d1f6753
Maria <m.gosp1704@gmail.com>
2026-02-24 19:29:50 +0200

    init

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { Image } from "expo-image";
+import { Platform, StyleSheet, Text, View } from "react-native";
+
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
